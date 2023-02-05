9d1f9dd9c95ee3a2fa6e431fe5374b107bd785ed
Akanksha1212 <singhakanksha221b@gmail.com>
2023-02-05 02:35:21 +0000

    UI updates in game screen

--- lib/screeens/Games.dart
+++ lib/screeens/Games.dart
@@ -1,6 +1,8 @@
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
-import 'Countdown.dart';
+import 'CountDown.dart';
 
 class Games extends StatefulWidget {
   const Games({super.key});
@@ -33,6 +35,75 @@ final List<ImageList> listofimages = [
 var index = 0;
 
 class _GamesState extends State<Games> {
+  CountDownController _controller = CountDownController();
+  bool _isPause = false;
+  bool _isReset = true;
+  PageController controller = PageController();
+
+  List<Widget> _list = <Widget>[
+    new Center(
+      child: Container(
+        child: Column(
+          children: [
+            ListTile(
+              leading: SizedBox(
+                child: listofimages[0].image,
+                height: 100,
+                width: 100,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+    new Center(
+      child: Container(
+        child: Column(
+          children: [
+            ListTile(
+              leading: SizedBox(
+                child: listofimages[1].image,
+                height: 100,
+                width: 100,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+    Center(
+      child: Container(
+        child: Column(
+          children: [
+            ListTile(
+              leading: SizedBox(
+                child: listofimages[2].image,
+                height: 100,
+                width: 100,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+    new Center(
+      child: Container(
+        child: Column(
+          children: [
+            ListTile(
+              leading: SizedBox(
+                child: listofimages[3].image,
+                height: 100,
+                width: 100,
+              ),
+              title: Text(listofimages[index].title),
+            ),
+          ],
+        ),
+      ),
+    )
+  ];
+  int _curr = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,41 +118,82 @@ class _GamesState extends State<Games> {
                   borderRadius: BorderRadius.only(
                       bottomLeft: Radius.circular(35),
                       bottomRight: Radius.circular(35))),
-              child: Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: ListTile(
-                          leading: SizedBox(
-                            child: listofimages[index].image,
-                            height: 100,
-                            width: 100,
-                          ),
-                          title: Text(listofimages[index].title),
+              child: PageView(
+                children: _list,
+                scrollDirection: Axis.horizontal,
+                controller: controller,
+                onPageChanged: (num) {
+                  setState(() {
+                    _curr = num;
+                    index++;
+                    if (index > 3) index = 0;
+                    _controller.start();
+                  });
+                },
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  CircularCountDownTimer(
+                    width: MediaQuery.of(context).size.width / 2,
+                    height: MediaQuery.of(context).size.height / 2,
+                    duration: 14,
+                    fillColor: Colors.amber,
+                    controller: _controller,
+                    backgroundColor: Colors.white54,
+                    strokeWidth: 10.0,
+                    strokeCap: StrokeCap.round,
+                    isTimerTextShown: true,
+                    isReverse: false,
+                    textStyle: TextStyle(fontSize: 50.0, color: Colors.black),
+                    ringColor: Colors.green,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(28.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        GestureDetector(
                           onTap: () {
                             setState(() {
-                              if (index <= 4) index++;
-                              if (index > 4) index = 0;
+                              if (_isPause) {
+                                _isPause = false;
+                                _controller.resume();
+                              } else {
+                                _isPause = true;
+                                _controller.pause();
+                              }
                             });
                           },
+                          child: Row(
+                            children: [
+                              Icon(_isPause ? Icons.play_arrow : Icons.pause),
+                              Text(_isPause ? 'Resume' : 'Pause')
+                            ],
+                          ),
                         ),
-                      ),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              if (_isReset) {
+                                _isReset = false;
+                                _controller.start();
+                              } else {
+                                _controller.start();
+                              }
+                            });
+                          },
+                          child: Row(
+                            children: [Icon(Icons.repeat), Text("Restart")],
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  )
+                ],
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: SizedBox(
-                child: Countdown(),
-                height: 200,
-                width: 200,
-              ),
-            ),
+            )
           ],
         ),
       ),
